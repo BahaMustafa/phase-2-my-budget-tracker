5aca3f13c4777db5f7e7e9d0170453317799bb38
= <=>
2023-10-05 00:58:56 -0700

    feat: Add search bar to ExpenseList component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,8 +3,8 @@ import './ExpenseList.css';
 
 function ExpenseList() {
   const [expenses, setExpenses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  
   useEffect(() => {
     fetch('http://localhost:5000/expenses')
       .then(response => response.json())
@@ -12,22 +12,49 @@ function ExpenseList() {
       .catch(error => console.error('Error fetching expenses:', error));
   }, []);
 
-  
+  const filteredExpenses = expenses.filter(expense =>
+    expense.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const deleteExpense = (id) => {
+    fetch(`http://localhost:5000/expenses/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(() => {
+        
+        setExpenses(expenses.filter(expense => expense.id !== id));
+      })
+      .catch(error => console.error('Error:', error));
+  };
   const totalExpense = expenses.reduce((total, expense) => {
     return total + parseFloat(expense.amount);
   }, 0);
-
   return (
-    <div className="expense-list">
-      <h2>Expenses</h2>
-      <p className="total">Total Expense: <span className="amount">${totalExpense.toFixed(2)}</span></p> 
-      {expenses.length === 0 ? (
-        <p>No expenses recorded.</p>
+    <div className="expense-list-container">
+      <h2 className="expense-list-title">Expenses</h2>
+      <input
+        type="text"
+        placeholder="Search expenses"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        className="search-bar"
+      />
+      <p className="total-expense">Total Expense: ${totalExpense.toFixed(2)}</p>
+      {filteredExpenses.length === 0 ? (
+        <p className="no-expenses-message">No expenses found.</p>
       ) : (
-        <ul>
-          {expenses.map(expense => (
-            <li key={expense.id}>
-              <strong>{expense.description}</strong> - ${parseFloat(expense.amount).toFixed(2)} on {expense.date}
+        <ul className="expenses-list">
+          {filteredExpenses.map(expense => (
+            <li className="expense-item" key={expense.id}>
+              <strong className="expense-description">{expense.description}</strong> - 
+              ${parseFloat(expense.amount).toFixed(2)} on {expense.date}
+              <button className="delete-button" onClick={() => deleteExpense(expense.id)}>Delete</button>
             </li>
           ))}
         </ul>
@@ -37,3 +64,9 @@ function ExpenseList() {
 }
 
 export default ExpenseList;
+
+
+
+
+
+
