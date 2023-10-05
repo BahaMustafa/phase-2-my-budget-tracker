808472b2c7b223dc2112fb5923fbf115aa192f34
= <=>
2023-10-05 00:30:42 -0700

    Add total expense calculation and styling

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,6 +4,7 @@ import './ExpenseList.css';
 function ExpenseList() {
   const [expenses, setExpenses] = useState([]);
 
+  
   useEffect(() => {
     fetch('http://localhost:5000/expenses')
       .then(response => response.json())
@@ -12,26 +13,14 @@ function ExpenseList() {
   }, []);
 
   
-  const deleteExpense = (id) => {
-    fetch(`http://localhost:5000/expenses/${id}`, {
-      method: 'DELETE',
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-      })
-      .then(() => {
-        
-        setExpenses(expenses.filter(expense => expense.id !== id));
-      })
-      .catch(error => console.error('Error:', error));
-  };
+  const totalExpense = expenses.reduce((total, expense) => {
+    return total + parseFloat(expense.amount);
+  }, 0);
 
   return (
     <div className="expense-list">
       <h2>Expenses</h2>
+      <p className="total">Total Expense: <span className="amount">${totalExpense.toFixed(2)}</span></p> 
       {expenses.length === 0 ? (
         <p>No expenses recorded.</p>
       ) : (
@@ -39,7 +28,6 @@ function ExpenseList() {
           {expenses.map(expense => (
             <li key={expense.id}>
               <strong>{expense.description}</strong> - ${parseFloat(expense.amount).toFixed(2)} on {expense.date}
-              <button onClick={() => deleteExpense(expense.id)}>Delete</button>
             </li>
           ))}
         </ul>
