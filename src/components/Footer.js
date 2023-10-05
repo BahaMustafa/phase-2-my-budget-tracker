030b89adca61ead0e39ed704c4d49ce5d21d25e4
= <=>
2023-10-05 04:18:34 -0700

    The Footer component now includes a copyright notice that dynamically updates the year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
+  const year = new Date().getFullYear(); 
+
   return (
     <footer className="footer">
-      
+      <p>&copy; {year} BudgetTracker. All Rights Reserved.</p>
     </footer>
   );
 }
