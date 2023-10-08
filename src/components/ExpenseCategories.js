09730b2d5f36863101fc37bf159a04b3e2929a8f
= <=>
2023-10-07 21:53:07 -0700

    categories mapping

--- src/components/ExpenseCategories.js
+++ src/components/ExpenseCategories.js
@@ -4,34 +4,34 @@ import './ExpenseCategories.css';
 
 function ExpenseCategories() {
 
+  const [expenses, setExpenses] = useState([]);
+
   useEffect(() => {
-    fetch(`http://localhost:5000/expenses/${id}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Expense not found');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setExpense(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Error fetching expense:', error);
-        setLoading(false);
-      });
-  }, [id]);
+    fetch('http://localhost:5000/expenses')
+      .then(response => response.json())
+      .then(data => setExpenses(data))
+      .catch(error => console.error('Error fetching expenses:', error));
+  }, []);
   
-  const categories = ['Food', 'Transport', 'Entertainment', 'Others']; 
+  const categories = ['Food', 'Transport', 'Entertainment', 'Housing/Rent', 'Others']; 
+
+  const getExpensesByCategory = (category) => {
+    return expenses.filter(expense => expense.category === category).map(expense => (
+      <li key={expense.id}>{expense.name}: ${expense.amount}</li>
+    ));
+  };
 
   return (
     <div>
       <h2>Categories</h2>
-      <ul>
-        {categories.map((category) => (
-          <li key={category}>{category}</li>  
-        ))}
-      </ul>
+      {categories.map((category) => (
+        <div key={category}>
+          <h3>{category}</h3>
+          <ul>
+            {getExpensesByCategory(category)}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
